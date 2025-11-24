ee4cb329d3b38aa689c178fc017e02ec7905d94b
infonautic.org <242398526+infonauticDotOrg@users.noreply.github.com>
2025-11-24 15:33:28 +0100

    Solve 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+    for (arg of itemsToRemove){
+        let result = array.filter(element => !itemsToRemove.includes(element));
+        return result;
+    }
 };
 
 // Do not edit below this line
