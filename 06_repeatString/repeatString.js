02267fca55e8c4b1777afb4eb6e3936d4ae1866b
infonautic.org <242398526+infonauticDotOrg@users.noreply.github.com>
2025-11-24 14:00:16 +0100

    Solve 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let arr = []
+    if(num < 0){
+        return 'ERROR';
+    }
+    for (let i=1; i<=num; i++){
+        arr.push(str)
+    }
+    let result = arr.reduce((add, current) => add + current, "")
+    console.log(result)
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
