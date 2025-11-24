27edd289e2c575b35914c7bc67eff846ecad1e1c
infonautic.org <242398526+infonauticDotOrg@users.noreply.github.com>
2025-11-24 16:18:38 +0100

    Solve 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let tempC = (tempF - 32)*5/9;;
+  let rounded = tempC.toFixed(1);
+  return Number(rounded);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let tempF = (tempC*9/5)+32;
+  let rounded = tempF.toFixed(1);
+  return Number(rounded);
 };
 
 // Do not edit below this line
