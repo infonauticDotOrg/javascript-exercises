4a30f7e9b30d4014edf1a323bd065eee702b6689
infonautic.org <242398526+infonauticDotOrg@users.noreply.github.com>
2025-11-24 14:47:23 +0100

    Solve 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversed = "";
+    let temp = [];
+    for (let i = 0; i < str.length; i++){
+        temp.push(str[i]);
+        reversed = temp.pop() + reversed;
+    }
+    console.log(reversed)
+    return reversed;
 };
 
 // Do not edit below this line
