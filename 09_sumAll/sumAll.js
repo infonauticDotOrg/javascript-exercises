05b09bfb51fbbac95d18df615150c69636ae1838
infonautic.org <242398526+infonauticDotOrg@users.noreply.github.com>
2025-11-24 15:56:28 +0100

    Solve 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(starting,ending){
+    let array = [];
+    if (ending - starting < 0 ){
+        let temp = starting;
+        starting = ending;
+        ending = temp;
+    }else if (ending < 0 || starting < 0) {
+        return "ERROR";
+    } else if (Number.isInteger(ending) != true || Number.isInteger(starting) != true) {
+        return "ERROR";
+    }
+    for (let i = starting; i <= ending; i++){
+        array.push(i)
+    }
+    let final = array.reduce((accumulator, current) => {
+        return accumulator + current;
+    })
+    return final
 
 };
 
